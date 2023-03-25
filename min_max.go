010fdcf7ecec311c250f70bdd860178166abd6c5
bbabb <brad.babb@boundedinfinity.com>
2023-03-25 00:00:35 -0500

    add Min, MinBy, Max, MaxBy

---
+++ min_max.go
@@ -0,0 +1,53 @@
+package slicer
+
+import (
+	c "golang.org/x/exp/constraints"
+)
+
+func Min[O c.Ordered](items ...O) (O, bool) {
+	fn := func(acc, curr O) O {
+		if acc < curr {
+			return acc
+		}
+
+		return curr
+	}
+
+	return Reduce(fn, items...)
+}
+
+func MinBy[I any, O c.Ordered](fn func(I) O, items ...I) (I, bool) {
+	by := func(acc, curr I) I {
+		if fn(acc) < fn(curr) {
+			return acc
+		}
+
+		return curr
+	}
+
+	return Reduce(by, items...)
+}
+
+func Max[O c.Ordered](items ...O) (O, bool) {
+	fn := func(acc, curr O) O {
+		if acc > curr {
+			return acc
+		}
+
+		return curr
+	}
+
+	return Reduce(fn, items...)
+}
+
+func MaxBy[I any, O c.Ordered](fn func(I) O, items ...I) (I, bool) {
+	by := func(acc, curr I) I {
+		if fn(acc) > fn(curr) {
+			return acc
+		}
+
+		return curr
+	}
+
+	return Reduce(by, items...)
+}
