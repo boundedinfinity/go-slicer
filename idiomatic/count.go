e3adeb4df1ce7de1e1f47d058e2aa7f98576bff6
bbabb <brad.babb@boundedinfinity.com>
2023-03-30 23:32:36 -0500

    refactor Count*

--- idiomatic/count.go
+++ idiomatic/count.go
@@ -1,42 +1,38 @@
 package idiomatic
 
 func Count[E any](fn func(E) bool, elems ...E) int {
-	wrapper := func(_ int, elem E) bool {
-		return fn(elem)
+	fn2 := func(_ int, elem E) (bool, error) {
+		return fn(elem), nil
 	}
-
-	return CountI(wrapper, elems...)
+	output, _ := CountErrI(fn2, elems...)
+	return output
 }
 
 func CountI[E any](fn func(int, E) bool, elems ...E) int {
-	var count int
-
-	for i, elem := range elems {
-		if fn(i, elem) {
-			count++
-		}
+	fn2 := func(i int, elem E) (bool, error) {
+		return fn(i, elem), nil
 	}
-
-	return count
+	output, _ := CountErrI(fn2, elems...)
+	return output
 }
 
 func CountErr[E any](fn func(E) (bool, error), elems ...E) (int, error) {
-	wrapper := func(_ int, elem E) (bool, error) {
+	fn2 := func(_ int, elem E) (bool, error) {
 		return fn(elem)
 	}
-
-	return CountErrI(wrapper, elems...)
+	return CountErrI(fn2, elems...)
 }
 
 func CountErrI[E any](fn func(int, E) (bool, error), elems ...E) (int, error) {
 	var count int
 	var err error
+	var ok bool
 
 	for i, elem := range elems {
-		ok, err := fn(i, elem)
+		ok, err = fn(i, elem)
 
 		if err != nil {
-			return count, err
+			break
 		}
 
 		if ok {
