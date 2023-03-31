8df333081a0594031fa00d934104a7fba5f9c289
bbabb <brad.babb@boundedinfinity.com>
2023-03-30 23:31:32 -0500

    refactor all, add some test

--- idiomatic/all.go
+++ idiomatic/all.go
@@ -1,42 +1,33 @@
 package idiomatic
 
-func All[T any](fn func(T) bool, elems ...T) bool {
-	for _, elem := range elems {
-		if !fn(elem) {
-			return false
-		}
+func All[E any](fn func(E) bool, elems ...E) bool {
+	fn2 := func(_ int, elem E) (bool, error) {
+		return fn(elem), nil
 	}
-
-	return true
+	output, _ := AllErrI(fn2, elems...)
+	return output
 }
 
-func AllI[T any](fn func(int, T) bool, elems ...T) bool {
-	for i, elem := range elems {
-		if !fn(i, elem) {
-			return false
-		}
+func AllI[E any](fn func(int, E) bool, elems ...E) bool {
+	fn2 := func(i int, elem E) (bool, error) {
+		return fn(i, elem), nil
 	}
-
-	return true
+	output, _ := AllErrI(fn2, elems...)
+	return output
 }
 
-func AllErr[T any](fn func(T) (bool, error), elems ...T) (bool, error) {
-	for _, elem := range elems {
-		ok, err := fn(elem)
-
-		if err != nil {
-			return false, err
-		}
-
-		if !ok {
-			return false, nil
-		}
+func AllErr[E any](fn func(E) (bool, error), elems ...E) (bool, error) {
+	fn2 := func(_ int, elem E) (bool, error) {
+		return fn(elem)
 	}
-
-	return true, nil
+	return AllErrI(fn2, elems...)
 }
 
-func AllErrI[T any](fn func(int, T) (bool, error), elems ...T) (bool, error) {
+func AllErrI[E any](fn func(int, E) (bool, error), elems ...E) (bool, error) {
+	if len(elems) < 1 {
+		return false, nil
+	}
+
 	for i, elem := range elems {
 		ok, err := fn(i, elem)
 
--- idiomatic/all_test.go
+++ idiomatic/all_test.go
@@ -1,1 +1,30 @@
 package idiomatic_test
+
+import (
+	"testing"
+
+	slicer "github.com/boundedinfinity/go-slicer/idiomatic"
+	"github.com/stretchr/testify/assert"
+)
+
+func allOk(s string) bool {
+	return s == "A"
+}
+
+func allOkI(_ int, s string) bool {
+	return s == "A"
+}
+
+func Test_All(t *testing.T) {
+	assert.False(t, slicer.All(allOk))
+	assert.True(t, slicer.All(allOk, "A", "A"))
+	assert.True(t, slicer.All(allOk, "A"))
+	assert.False(t, slicer.All(allOk, "A", "B"))
+}
+
+func Test_AllI(t *testing.T) {
+	assert.False(t, slicer.AllI(allOkI))
+	assert.True(t, slicer.AllI(allOkI, "A", "A"))
+	assert.True(t, slicer.AllI(allOkI, "A"))
+	assert.False(t, slicer.AllI(allOkI, "A", "B"))
+}
